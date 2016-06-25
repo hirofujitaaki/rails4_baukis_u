9268dd28e808d896a029bba60e9ba1b81f514f4f
hiforujitaaki <hirofujitaaki.gmail.com>
2016-06-25 15:44:17 +0900

    chapter8-4 complete

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe StaffMember do
+  describe '#password=' do
+    it 'returns a string of 60 characters if a string is given.' do
+      member = StaffMember.new
+      member.password = 'baukis'
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+
+    example 'hashed_password returns nil if nil is give.' do
+      member = StaffMember.new(hashed_password: 'x')
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
+  end
 end
