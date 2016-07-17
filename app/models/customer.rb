4c9a91cc86d6d1e7c9bfedc9e9ccbe4fe44b76a5
hiforujitaaki <hirofujitaaki.gmail.com>
2016-07-17 09:46:59 +0900

    until p479

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,13 @@
 class Customer < ActiveRecord::Base
+  include EmailHolder
   include PersonalNameHolder
+  include PasswordHolder
 
   has_one :home_address, dependent: :destroy, autosave: true
   has_one :work_address, dependent: :destroy, autosave: true
-
-  before_validation do
-    self.email_for_index = email.downcase if email
-  end
+  has_many :phones, dependent: :destroy
+  has_many :personal_phones, -> { where(address_id: nil).order(:id) },
+    class_name: 'Phone', autosave: true
 
   validates :gender, inclusion: { in: %w(male female), allow_blank: true }
   validates :birthday, date: {
@@ -14,12 +15,4 @@ class Customer < ActiveRecord::Base
     before: ->(obj) { Date.today },
     allow_blank: true
   }
-
-  def password=(raw_password)
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
 end
